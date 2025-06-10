1d3c5f0cc53c8359645ebf92f2c9881f5d0815ef
ksj321456 <ksj321456@gmail.com>
2025-06-10 18:29:42 +0900

    2752 StringTokenzier 메소드와 forEach 사용

---
+++ src/Q2752/Main.java
@@ -0,0 +1,21 @@
+package Q2752;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        list.forEach(num -> System.out.print(num + " "));
+        System.out.println();
+    }
+}
