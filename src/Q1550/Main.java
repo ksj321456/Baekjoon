3af694936c50c1fd03f6137baec14c10e082d25c
ksj321456 <ksj321456@gmail.com>
2025-06-10 17:38:06 +0900

    1550

--- src/Q1550/Main.java
+++ src/Q1550/Main.java
@@ -1,4 +1,13 @@
 package Q1550;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String hex = br.readLine();  // 한 줄 전체 입력 (ex: A, 1F, 10AB3 등)
+
+        int decimal = Integer.parseInt(hex, 16);  // 16진수 문자열 → 10진수 int
+        System.out.println(decimal);
+    }
 }
